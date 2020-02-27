7a0e928a9d98ade2d5af4c1ec3c22e1c1effbb33
Sarah Fleming <JJ10334@tn.gov>
2020-02-27 09:30:58 -0600

    added template html and component for product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
+  // A component can only belong to one Angular module
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
+  // Imports below is where you bring in any external modules that your app needs
   imports: [
     BrowserModule
   ],
+  // Bootstrap below lists the starting component for the application
   bootstrap: [AppComponent]
 })
 export class AppModule { }
