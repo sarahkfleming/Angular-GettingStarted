eb5c5d0239c01aa3843879e2fe77290942e12aeb
Sarah Fleming <JJ10334@tn.gov>
2020-02-28 14:09:11 -0600

    added product data service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -13,6 +14,7 @@ export class ProductListComponent
     imageWidth = 50;
     imageMargin = 2;
     showImage = false;
+    private _productService;
     _listFilter: string;
     get listFilter(): string {
       return this._listFilter;
@@ -23,42 +25,11 @@ export class ProductListComponent
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2019",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-          },
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          }
-    ];
+    products: IProduct[] = [];
 
-    constructor() {
-      this.filteredProducts = this.products;
-      this.listFilter = 'cart';
+    // Put as little as possible in the constructor function
+    constructor(productService: ProductService) {
+      this._productService = productService;
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -72,12 +43,12 @@ export class ProductListComponent
     }
 
     ngOnInit(): void {
-      console.log('In OnInit');
+      this.products = this._productService.getProducts();
+      this.filteredProducts = this.products;
     }
 
     onRatingClicked(message: string): void {
       this.pageTitle = 'Product List: ' + message;
 
     }
-
   }
