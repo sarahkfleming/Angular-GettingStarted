2a39ff13ac7fe89c3a45858871029585f94db61a
Sarah Fleming <JJ10334@tn.gov>
2020-02-28 14:37:08 -0600

    added error handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -18,7 +19,8 @@ import { StarComponent } from './shared/star.component';
   // Imports below is where you bring in any external modules that your app needs
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   // Bootstrap below lists the starting component for the application
   bootstrap: [AppComponent]
